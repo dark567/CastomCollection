7e795a5f4313282d18ef5f3fc45350ecc8c8fd99
dark567 <darkmit@list.ru>
2019-10-13 21:07:48 +0300

    201910132105

---
+++ ConsoleApp1/Logic/UserCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Logic
+{
+    public class UserCollection
+    {
+        List<User> user = new List<User>
+        {
+            new User(name:"Tom" , age: 27),
+            new User(name:"Clark", age:30),
+            new User(name:"Serg", age:39)
+        };
+
+        int position = -1;
+        public bool MoveNext()
+        {
+            if (position < user.Count-1)
+            {
+                position++;
+                return true;
+            }
+            Reset();
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+        public object Current
+        {
+            get { return user[position]; }
+        }
+
+        public UserCollection GetEnumerator()
+        {
+            return this;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Реализация кастомной коллекции без IEnumerable и IEmumerator
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var users = new UserCollection();
+
+            foreach (User item in users)
+            {
+                Console.WriteLine($"{item.Id}\t{item.Name}\t{item.Age}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
